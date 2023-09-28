233f6edb870e979706f7135aface3a2b937cf713
Z <zmfisher01@gmail.com>
2023-09-28 20:13:35 +0000

    Fleshed out user input and stores variable. Test print currently.

--- geneticTSP.py
+++ geneticTSP.py
@@ -18,7 +18,7 @@ def printCities(cities):
     for city in cities:
         print("{:6}\t{:>6.2f}\t{:>6.2f}".format(city[0],city[1],city[2]))
 
-def initRandCities(cityCount = DEFAULT_CITY_COUNT):
+def initRandCities(cityCount):
     """Returns initialized list of random cities."""
     cities = []
     max_x_axis = 200
@@ -30,16 +30,47 @@ def initRandCities(cityCount = DEFAULT_CITY_COUNT):
         cities.append((city_name, x, y))
     return cities
 
+def queryUser(query, typeDesired, defaultValue = False):
+    """Queries the user, for a specific type, can accept default value"""
+    while(1):
+        print(query, end = " ")
+        userInput = input()
+        try:
+
+            if (len(userInput) == 0 and defaultValue):
+                return defaultValue
+            elif (typeDesired == "string"):
+                return userInput
+            elif (typeDesired == "int"):
+                return int(userInput)
+            elif (typeDesired == "float"):
+                return float(userInput)
+            elif (typeDesired == "bool"):
+                if (userInput.lower() == "y"):
+                    return True
+                elif (userInput.lower() == "n"):
+                    return False
+        except ValueError:
+            print("Please enter", typeDesired)
+
 if __name__ == "__main__":
     print("Welcome to a Genetic Algorithm solver for  the TSP")
-    while(1):
-        print("How many cities do you want? (Enter for default (", DEFAULT_CITY_COUNT ,")): ", sep = "" , end = "")
-        userCityCount = input()
-        if (userCityCount.isdigit()):
-            random_cities = initRandCities(int(userCityCount))
-            break
-        elif (len(userCityCount) == 0):
-            random_cities = initRandCities()
-            break
+
+    cityCount = queryUser(("How many cities do you want? (Enter for default ("+ str(DEFAULT_CITY_COUNT) + ")) (int)"), "int", DEFAULT_CITY_COUNT)
+    random_cities = initRandCities(cityCount)
+
+    print("Cities generated!")
     printCities(random_cities)
+
+    mutationRate = queryUser("What mutation rate do you want (float)?", "float")
+
+    childProportion = queryUser("What proportion of new children do you want (float)? ", "float")
+
+    stopOnStagnation = queryUser("Stop the algorithm on stagnation? (y/n)", "bool")
+
+    whenIsStagnation = 0
+    if(stopOnStagnation):
+        whenIsStagnation = queryUser("How many iterations before stagnation is considered? (int)", "int")
+
+    print("TEST: ", mutationRate, childProportion, stopOnStagnation, whenIsStagnation)
 
